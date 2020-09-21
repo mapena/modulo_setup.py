18cfb5cdc80aefb75073f064041bf9fbd7912ed8
mapena <mapena@gmail.com>
2020-09-21 02:48:03 -0300

    .

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-from setuptools import setup
+#from setuptools import setup
+import setuptools
 
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+#packages=setuptools.find_packages(),
 setup(
       name="paquete_suma_resta",
       version="1.0.0",
@@ -7,7 +11,10 @@ setup(
       author="mp",
       author_email="mp@mp.com.ar",
       url="www.sarasa.com.ar",
-      packages=["libreria","libreria.libresta"]
-
+      packages=["libreria","libreria.libresta"],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+      ]
 
 )
